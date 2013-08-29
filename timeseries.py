e826763548824df87db8a2897ff26daa9542945e
ggear <graham.gear@gmail.com>
2013-08-29 23:21:07 +0100

    Update license

--- timeseries.py
+++ timeseries.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
+# Licensed to Cloudera, Inc. under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  Cloudera, Inc. licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Originally adapted from timeseries.py as part of the cm_api module
+#
+#     https://github.com/cloudera/cm_api/blob/cm-4.6/python/examples/timeseries.py
 
 """
 Provide the aggregated timeseries for a given query in CSV format
